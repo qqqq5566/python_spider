43e5a214f7c9125954b8199124c7a8ad7fd6b733
qqqq55566 <sun_sheg_gang@163.com>
2018-11-09 18:15:01 +0800

    修改website/run.py

--- website/run.py
+++ website/run.py
@@ -1,11 +1,4 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
+from scrapy.cmdline import execute
 
-from spiders.dmoz_spider import DmozSpider
 
-settings = get_project_settings()
-process = CrawlerProcess(settings=settings)
-
-process.crawl(DmozSpider)
-
-process.start()
+execute(['scarpy', 'crawl', 'lianmishu'])
